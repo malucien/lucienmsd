58f32361c463c1532a88cfceb44bccb44bca2fc6
malucien <124107514+malucien@users.noreply.github.com>
2023-03-06 21:49:39 -0600

    Third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,24 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
   return (
     <nav
       className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex jsutify-between items-center max-w-7xl max-auto">
-
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0,0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer ">Lucien Masudi</p>
+        </Link> 
       </div>
     </nav>
   )
