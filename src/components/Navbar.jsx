9ae45fa3fc6a1c8cb58686ae826009df0e14fbfe
malucien <124107514+malucien@users.noreply.github.com>
2023-03-06 19:57:58 -0600

    Second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav
+      className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex jsutify-between items-center max-w-7xl max-auto">
+
+      </div>
+    </nav>
   )
 }
 
