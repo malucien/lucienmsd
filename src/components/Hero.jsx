714524fbd7c7e4386909e59b4040a2c5835743ca
malucien <124107514+malucien@users.noreply.github.com>
2023-03-07 20:21:38 -0600

    First commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen ma-auto'>
+      <div className={`{styles.paddingX} absolute insert-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff] ml-12" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient ml-12"/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>Bonjour, I'm <span className="text-[#915eff]">Lucien</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>"I create innovative and user-friendly websites and applications."</p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer ">Lucien <span className="sm:block hidden"> Masudi</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer ">Lucien &nbsp; <span className="sm:block hidden"> Masudi</span></p>
         </Link> 
         <ul className="list-none float-right md:float-left hidden sm:flex flex-row gap-10 justify-end items-center ml-auto">
           {navLinks.map((Link) =>(
