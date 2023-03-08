3cda65ed7f945c6f7d3b652a7202c1bf93016932
malucien <124107514+malucien@users.noreply.github.com>
2023-03-07 19:17:35 -0600

    First commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,24 @@ const Navbar = () => {
               <div className={`${!toggle
                 ? "hidden"
                 :"flex" } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-
+                <ul className="list-none flex justify-end items-start flex-col gap-4">
+                {navLinks.map((Link) =>(
+                  <li
+                    key={Link.id}
+                    className={`${
+                      active === Link.title
+                        ? "text-white"
+                        :"text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(Link.title)
+                    }}
+                  >
+                    <a href={`#${Link.id}`}>{Link.title}</a>
+                  </li>
+                ))}
+                </ul>
               </div>
         </div>
       </div>
