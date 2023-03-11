c3761584ba96d97e00aa61722ede6b9882715644
malucien <124107514+malucien@users.noreply.github.com>
2023-03-11 12:05:28 -0600

    First commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-
+import {SectionWrapper} from "../hoc"
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
