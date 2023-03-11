c06c9475dc9c1faf96f25edb8eee73a0e7ce94dd
malucien <124107514+malucien@users.noreply.github.com>
2023-03-10 18:44:51 -0600

    First commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,16 @@ const Hero = () => {
       <div className={`{styles.paddingX} absolute insert-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff] ml-12" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient ml-12"/>
+          <div className="w-1 sm:h-80 h-50 violet-gradient ml-12"/>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-[#915eff]">Lucien</span></h1>
+          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className="text-[#68b8ed]">Lucien</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>"I create innovative and user-friendly  <br className="sm:block hidden" /> websites and applications."</p>
         </div>
       </div>
       <ComputersCanvas/>
 
-      <div className="absolute xs:bottom-10 buttom-32 w-full flex justif" ></div>
+      <div className="absolute xs:bottom-10 buttom-32 w-full flex justify" ></div>
     </section>
   )
 }
