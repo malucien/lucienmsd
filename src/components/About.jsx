180bc721fa22d86f31079b820168d07887db61d0
malucien <124107514+malucien@users.noreply.github.com>
2023-03-11 11:56:03 -0600

    First commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import tilt from 'react-tilt';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants/index';
-import {fadeIn, textVariant } from '../utils/motion';
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-const ServiceCard = ({index, title, icon}) => {
-  return(
-    <p>{title}</p>
-  )
-}
+import { styles } from "../styles";
+import { services } from "../constants";
+
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-[#2b6f80] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
