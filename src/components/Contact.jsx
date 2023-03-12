03fc44e838717727efbc4141cf4e840f601cc87c
malucien <124107514+malucien@users.noreply.github.com>
2023-03-12 13:39:24 -0500

    First commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// template_pra0e9h
+//service_r5o6tuo
+//RpNKw4xntPu133bFF
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,21 +37,21 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_r5o6tuo",
+        "template_pra0e9h",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Lucien masudi",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "masudilucien44@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "RpNKw4xntPu133bFF"
       )
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Hello, thank you for contacting me. I will get back to you as soon as possible. Have a wonderfull day !");
 
           setForm({
             name: "",
